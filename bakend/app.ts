a07727b834cfb32820e8f1c67036814f60c6225f
mampionona33 <ramamps33@gmail.com>
2023-03-24 12:43:56 +0300

    start implementing auth facebook

--- bakend/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, NextFunction, Request, Response } from 'express';
-const app: Express = express();
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('response form back');
-  next();
-});
-
-export default app;
