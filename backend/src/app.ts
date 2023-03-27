bf7cc8bae845601c66cea711d0876c9b5b0ae266
mampionona33 <ramamps33@gmail.com>
2023-03-27 14:26:47 +0300

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,10 @@ import { router as userRoutes } from './routes/userRoutes';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send('response form back');
   next();
 });
 
-app.use(process.env.USER_BASED_URL, userRoutes);
+app.use('/api/v1/users', userRoutes);
 
 export default app;
