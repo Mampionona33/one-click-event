f7e40469928d7204846f683abd1442ea22080c09
mampionona33 <ramamps33@gmail.com>
2023-03-28 09:38:55 +0300

    finish to setting up backend deployment on vercel by using rimraf and pre-commit package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { router as userRoutes } from './routes/userRoutes';
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('response form back test 123');
+  res.send('response form back  123');
   next();
 });
 
